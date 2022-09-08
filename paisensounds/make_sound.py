5ee9789e72dd1e1d40fed0f4fd19cab9aeffb45f
AlessandroColombo0 <92480131+AlessandroColombo0@users.noreply.github.com>
2022-09-08 19:08:29 +0200

    Update make_sound.py

--- paisensounds/make_sound.py
+++ paisensounds/make_sound.py
@@ -4,6 +4,6 @@ import simpleaudio as sa
 def beep():
     
 
-    wave_obj = sa.WaveObject.from_wave_file("/Users/alessandro/PycharmProjects/ZZZ-Futuristic Computer Sound Effects All Sounds")
+    wave_obj = sa.WaveObject.from_wave_file("/Users/alessandro/PycharmProjects/ZZZ-Futuristic Computer Sound Effects All Sounds.wav")
     play_obj = wave_obj.play()
     play_obj.wait_done()
